7df4cd0f826e1567828312d36340770a873ef13f
Emil Thranholm <ethranholm@hotmail.com>
2025-03-02 18:57:10 +0100

    dst-mail

--- send mail.R
+++ send mail.R
@@ -21,8 +21,8 @@ attachments <- map_chr(update_ics$summary, ~paste0("ics_files/",
                                                      " "), ".ics"))
 
 test_email <- gm_mime() %>% 
-  # gm_to(secret_decrypt(Sys.getenv("DST_MAIL"), "FODBOLD_KEY")) %>%
-  gm_to(secret_decrypt("59FM83CiJC9ZZbrJqw3YufDeX7I8e5wNEk2mw-uHxQsgRjZebtg", "FODBOLD_KEY")) %>% 
+  gm_to(secret_decrypt("EoBIB9DK5t2Xwy4X8qAFNp8LiUyzoRavNp8", "FODBOLD_KEY")) %>%
+  # gm_to(secret_decrypt("59FM83CiJC9ZZbrJqw3YufDeX7I8e5wNEk2mw-uHxQsgRjZebtg", "FODBOLD_KEY")) %>% 
   gm_subject("Sport næste måned") %>% 
   gm_text_body("Se vedhæftet") %>% 
   reduce(.init = ., .x = attachments, .f = attach_multiple)
