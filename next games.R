74a78a893de193aae4bc573af8aced015dcfa4d7
Emil Thranholm <ethranholm@hotmail.com>
2025-03-04 21:11:15 +0100

    test print

--- next games.R
+++ next games.R
@@ -66,28 +66,32 @@ exist_ics <- map(paste0("ics_files/", list.files("ics_files")), ic_read) %>%
   select(-any_of("UID")) %>% 
   rename_with(str_to_lower)
 
-cat(exist_ics$summary)
-# 
-# new_ics <- next_month %>% 
-#   rename(dtstart = startTimestamp,
-#          dtend = end_time) %>% 
-#   select(dtstart, dtend, summary) %>% 
-#   mutate(class = "PRIVATE")
-# 
+cat(paste0(exist_ics$summary, exist_ics$dtstart, collapse = ", "))
+
+new_ics <- next_month %>%
+  rename(dtstart = startTimestamp,
+         dtend = end_time) %>%
+  select(dtstart, dtend, summary) %>%
+  mutate(class = "PRIVATE")
+
+cat(paste0(new_ics$summary, new_ics$dtstart, collapse = ", "))
+
+
+## TODO::: måske driller tidszonerne
 # if (nrow(exist_ics) > 0) update_ics <- setdiff(new_ics, exist_ics) else update_ics <- new_ics
 # 
 # if (nrow(update_ics) > 1) {
 #   for (i in 1:nrow(update_ics)){
-#     
+# 
 #     ic_event(start_time = update_ics$dtstart[i],
 #              end_time = update_ics$dtend[i],
 #              summary = update_ics$summary[i],
 #              more_properties = TRUE,
-#              event_properties = c("CLASS" = "PRIVATE")) %>% 
+#              event_properties = c("CLASS" = "PRIVATE")) %>%
 #       ic_write(paste0("ics_files/", str_remove_all(
 #         str_replace(update_ics$summary[i], "-", "_"),
 #         " "), ".ics"))
-#     
+# 
 #   }
 # }
 
