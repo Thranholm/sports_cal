2dc3a2bee69290f9d99b016094492ffe60b4ec72
Emil Thranholm <ethranholm@hotmail.com>
2025-03-04 22:03:39 +0100

    back to prod

--- send mail.R
+++ send mail.R
@@ -21,13 +21,11 @@ attachments <- map_chr(update_ics$summary, ~paste0("ics_files/",
                                                      " "), ".ics"))
 
 test_email <- gm_mime() %>% 
-  #gm_to(secret_decrypt("EoBIB9DK5t2Xwy4X8qAFNp8LiUyzoRavNp8", "FODBOLD_KEY")) %>%
-  gm_to(secret_decrypt("59FM83CiJC9ZZbrJqw3YufDeX7I8e5wNEk2mw-uHxQsgRjZebtg", "FODBOLD_KEY")) %>% 
+  gm_to(secret_decrypt("EoBIB9DK5t2Xwy4X8qAFNp8LiUyzoRavNp8", "FODBOLD_KEY")) %>%
+  # gm_to(secret_decrypt("59FM83CiJC9ZZbrJqw3YufDeX7I8e5wNEk2mw-uHxQsgRjZebtg", "FODBOLD_KEY")) %>% 
   gm_subject("Sport næste måned") %>% 
   gm_text_body("Se vedhæftet") %>%
-  gm_text_body(paste0(update_ics$summary, collapse = ","))
-# %>% 
-#   reduce(.init = ., .x = attachments, .f = attach_multiple)
+  reduce(.init = ., .x = attachments, .f = attach_multiple)
 
 gm_send_message(test_email)
 
